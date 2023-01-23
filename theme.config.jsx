86f15b2854f476761fbf395f4bb4fd4689e3a0cd
Nick Costello <nick@nickcostello.dev>
2023-01-22 21:09:01 -0500

    Configure and add initial index

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,31 @@
+import { useConfig } from "nextra-theme-docs";
+
 export default {
-  logo: <span>Nick Costello</span>,
+  editLink: {
+    text: "",
+  },
+  favicon: "./public/favicon.ico",
+  feedback: {
+    content: "Provide me feedback →",
+    useLink: () => "https://twitter.com/intent/tweet?text=@nickmcostello",
+  },
+  footer: {
+    text: <span>© Copyright {new Date().getFullYear()} Nick Costello</span>,
+  },
+  logo: (
+    <span>
+      <b>Nick Costello</b>
+    </span>
+  ),
   project: {
     link: "https://github.com/nickmcostello",
+  },
+  search: {
+    placeholder: "Search for something",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s",
+    };
   },
 };
